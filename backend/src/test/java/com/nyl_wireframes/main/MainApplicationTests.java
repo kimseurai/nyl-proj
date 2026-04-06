10a4c1e58c70dc5360fe291735ac8385bf2f8f73
Ben Macaro <benedikt.macaro@capgemini.com>
2026-04-06 10:50:24 -0400

    add backend files

---
+++ backend/src/test/java/com/nyl_wireframes/main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyl_wireframes.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
